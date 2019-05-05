0b1c6eb37468e599cd3fa539c52052066236e0d7
Diwen.Liu <demon90s@163.com>
2019-05-05 20:15:36 +0800

    十步杀一人，千里不留行

--- mount_dir.sh
+++ mount_dir.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+# 挂载远程目录
+# usage: ./mount_dir remote_dir local_dir
+
 source ./detail/config.sh
 
 f_main()
 {
-	sshfs -p ${public_port} ${agent_user}@${public_ip}:$1 $2
+	sshfs -p ${public_port} IdentityFile=${rsa_file} ${agent_user}@${public_ip}:$1 $2
 }
 
 f_main $@
